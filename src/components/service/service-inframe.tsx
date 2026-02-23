82690c03140c4db7a434918ef2730fd6b964c062
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2026-02-23 17:13:31 +0300

    fix: sercvice responsiveness

--- src/components/service/service-inframe.tsx
+++ src/components/service/service-inframe.tsx
@@ -115,7 +115,7 @@ export function ServiceInframe() {
 
         {/* Platforms section */}
         <div className="py-5">
-          <div className="mx-auto mb-5" id="see-our-works" >
+          <div className="mx-auto mb-5" id="see-our-works">
             <p
               className="fw-semibold text-black text-center"
               style={{
@@ -144,37 +144,6 @@ export function ServiceInframe() {
             View Our Portfolio <span>&gt;</span>
           </a> */}
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            <Link
-              href="/projects"
-              className="text-primary fw-medium"
-              style={{ marginLeft: "0.8em", fontSize:'19px' }}
-            >
-              View Our Portfolio
-              <i className="fa-solid fa-angle-right learn-more-icon"></i>
-            </Link>
-
-          {/* Platform icons */}
-          <div className="row justify-content-center g-4 mx-auto" style={{ marginTop: "38px"}}>
-            {[
-              { name: "YouTube", icon: "fab fa-youtube" },
-              { name: "TikTok", icon: "fab fa-tiktok" },
-              { name: "Instagram", icon: "fab fa-instagram" },
-              { name: "Facebook", icon: "fab fa-facebook" },
-              // { name: "Behance", icon: "fab fa-behance" },
-            ].map((item) => (
-              <div key={item.name} className="col-6 col-md-4 col-lg-2 text-center">
-                <i className={`${item.icon} fs-1 text-dark mb-2`}></i>
-                <div className="fw-medium">{item.name}</div>
-              </div>
-            ))}
-          </div>
-
-
-=======
-=======
->>>>>>> Stashed changes
           <Link
             href="/projects"
             className="text-primary fw-medium"
@@ -183,10 +152,37 @@ export function ServiceInframe() {
             View Our Portfolio
             <i className="fa-solid fa-angle-right learn-more-icon"></i>
           </Link>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
+          {/* Platform icons */}
+          <div
+            className="row justify-content-center g-4 mx-auto"
+            style={{ marginTop: "38px" }}
+          >
+            {[
+              { name: "YouTube", icon: "fab fa-youtube" },
+              { name: "TikTok", icon: "fab fa-tiktok" },
+              { name: "Instagram", icon: "fab fa-instagram" },
+              { name: "Facebook", icon: "fab fa-facebook" },
+              // { name: "Behance", icon: "fab fa-behance" },
+            ].map((item) => (
+              <div
+                key={item.name}
+                className="col-6 col-md-4 col-lg-2 text-center"
+              >
+                <i className={`${item.icon} fs-1 text-dark mb-2`}></i>
+                <div className="fw-medium">{item.name}</div>
+              </div>
+            ))}
+          </div>
+
+          <Link
+            href="/projects"
+            className="text-primary fw-medium"
+            style={{ marginLeft: "0.8em", fontSize: "19px" }}
+          >
+            View Our Portfolio
+            <i className="fa-solid fa-angle-right learn-more-icon"></i>
+          </Link>
 
           {/* Platform icons */}
           <div
@@ -234,18 +230,18 @@ export function ServiceInframe() {
         </div>
       </div>
       <style jsx>{`
-          #service-section-title {
-            font-size: 80px;
-          }
-          #see-our-works{
-            max-width:41.666%;
-          }
+        #service-section-title {
+          font-size: 80px;
+        }
+        #see-our-works {
+          max-width: 41.666%;
+        }
         @media (max-width: 600px) {
           #service-section-title {
-            font-size: 60px ;
+            font-size: 60px;
           }
-          #see-our-works{
-            max-width:85%;
+          #see-our-works {
+            max-width: 85%;
           }
         }
       `}</style>
